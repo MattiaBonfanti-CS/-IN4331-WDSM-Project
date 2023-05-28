69d7e3f70798227588417beef09f2c4b763593d8
Mattia Bonfanti <bonfantimattia@gmail.com>
2023-05-28 15:15:26 +0200

    Add stock db k8s sharding
    - Add k8s configuration for stock db sharding

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -6,8 +6,12 @@ minikube image load order:latest stock:latest user:latest
 
 echo "Installing Redis Helm Charts for the order, payment and stock databases..."
 helm install -f helm-config/redis-helm-values.yaml order-db bitnami/redis --set master.service.nodePorts.redis=30000 --set replica.service.nodePorts.redis=30001
+
 helm install -f helm-config/redis-helm-values.yaml payment-db bitnami/redis --set master.service.nodePorts.redis=30100 --set replica.service.nodePorts.redis=30101
-helm install -f helm-config/redis-helm-values.yaml stock-db bitnami/redis --set master.service.nodePorts.redis=30200 --set replica.service.nodePorts.redis=30201
+
+helm install -f helm-config/redis-helm-values.yaml stock-db-0 bitnami/redis --set master.service.nodePorts.redis=30200 --set replica.service.nodePorts.redis=30201
+helm install -f helm-config/redis-helm-values.yaml stock-db-1 bitnami/redis --set master.service.nodePorts.redis=30210 --set replica.service.nodePorts.redis=30211
+helm install -f helm-config/redis-helm-values.yaml stock-db-2 bitnami/redis --set master.service.nodePorts.redis=30220 --set replica.service.nodePorts.redis=30221
 
 echo "Applying k8s yml files..."
 kubectl apply -f k8s
--- remove-charts-minikube.sh
+++ remove-charts-minikube.sh
@@ -5,8 +5,21 @@ kubectl delete -f k8s
 
 echo "Uninstalling Redis Helm Chart..."
 helm uninstall order-db
+
 helm uninstall payment-db
-helm uninstall stock-db
+
+helm uninstall stock-db-0
+helm uninstall stock-db-1
+helm uninstall stock-db-2
 
 echo "Removing volumes..."
-kubectl delete pvc redis-data-order-db-redis-master-0 redis-data-order-db-redis-replicas-0 redis-data-payment-db-redis-master-0 redis-data-payment-db-redis-replicas-0 redis-data-stock-db-redis-master-0 redis-data-stock-db-redis-replicas-0
+kubectl delete pvc redis-data-order-db-redis-master-0 \
+                   redis-data-order-db-redis-replicas-0 \
+                   redis-data-payment-db-redis-master-0 \
+                   redis-data-payment-db-redis-replicas-0 \
+                   redis-data-stock-db-0-redis-master-0 \
+                   redis-data-stock-db-0-redis-replicas-0 \
+                   redis-data-stock-db-1-redis-master-0 \
+                   redis-data-stock-db-1-redis-replicas-0 \
+                   redis-data-stock-db-2-redis-master-0 \
+                   redis-data-stock-db-2-redis-replicas-0
