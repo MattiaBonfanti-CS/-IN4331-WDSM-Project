fa1cdf5508bace944896c9ec2a696ea917882af4
Dyon van der Ende <dyonx@hotmail.com>
2023-05-30 11:47:45 +0200

    fixed missing payment shards for docker and minikube

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -7,7 +7,9 @@ minikube image load order:latest stock:latest user:latest
 echo "Installing Redis Helm Charts for the order, payment and stock databases..."
 helm install -f helm-config/redis-helm-values.yaml order-db bitnami/redis --set master.service.nodePorts.redis=30000 --set replica.service.nodePorts.redis=30001
 
-helm install -f helm-config/redis-helm-values.yaml payment-db bitnami/redis --set master.service.nodePorts.redis=30100 --set replica.service.nodePorts.redis=30101
+helm install -f helm-config/redis-helm-values.yaml payment-db-0 bitnami/redis --set master.service.nodePorts.redis=30100 --set replica.service.nodePorts.redis=30101
+helm install -f helm-config/redis-helm-values.yaml payment-db-1 bitnami/redis --set master.service.nodePorts.redis=30110 --set replica.service.nodePorts.redis=30111
+helm install -f helm-config/redis-helm-values.yaml payment-db-2 bitnami/redis --set master.service.nodePorts.redis=30120 --set replica.service.nodePorts.redis=30121
 
 helm install -f helm-config/redis-helm-values.yaml stock-db-0 bitnami/redis --set master.service.nodePorts.redis=30200 --set replica.service.nodePorts.redis=30201
 helm install -f helm-config/redis-helm-values.yaml stock-db-1 bitnami/redis --set master.service.nodePorts.redis=30210 --set replica.service.nodePorts.redis=30211
